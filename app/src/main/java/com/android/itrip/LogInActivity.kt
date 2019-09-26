d1f1d5c3c84dc4e7dc195b3f05fcc01ad8c06f9d
Facundo Aita <facundo91@gmail.com>
2019-09-26 00:38:04 -0300

    Sign Out

--- app/src/main/java/com/android/itrip/LogInActivity.kt
+++ app/src/main/java/com/android/itrip/LogInActivity.kt
@@ -4,9 +4,17 @@ import android.content.Intent
 import android.os.Bundle
 import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.android.itrip.util.NukeSSLCerts
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
 import com.firebase.ui.auth.AuthUI
 import com.firebase.ui.auth.IdpResponse
 import com.google.firebase.auth.FirebaseAuth
+import org.json.JSONObject
+import java.util.logging.Logger
 
 
 class LogInActivity : AppCompatActivity() {
@@ -16,22 +24,46 @@ class LogInActivity : AppCompatActivity() {
         AuthUI.IdpConfig.EmailBuilder().build(),
 //        AuthUI.IdpConfig.PhoneBuilder().build(),
 //        AuthUI.IdpConfig.FacebookBuilder().build(),
-        AuthUI.IdpConfig.GoogleBuilder().build())
+        AuthUI.IdpConfig.GoogleBuilder().build()
+    )
+    val logger = Logger.getLogger(LogInActivity::class.java.name)
 
+    fun getApiToken(auth: FirebaseAuth?) {
+        val queue = Volley.newRequestQueue(this)
+        val base_api_url = resources.getString(R.string.base_api_url)
+        val url = base_api_url + "token/"
+        var accessToken = ""
+        var refreshToken = ""
+        val updateJsonobj = JSONObject()
+        updateJsonobj.put("username", "admin")
+        updateJsonobj.put("password", "SlaidTeam123")
+
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url, updateJsonobj,
+            Response.Listener { response ->
+                logger.info("Response: %s".format(response.toString()))
+                refreshToken = response.getString("refresh")
+                accessToken = response.getString("access")
+                auth?.signInWithCustomToken(accessToken)
+            },
+            Response.ErrorListener { response -> logger.info("That didn't work! " + response.message) })
+
+        queue.add(jsonObjectRequest)
+    }
 
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
-
+        NukeSSLCerts().nuke()
         val auth = FirebaseAuth.getInstance()
+        getApiToken(auth)
         if (auth.currentUser != null) {
             // already signed in
             val intent = Intent(this, MainActivity::class.java).apply {
                 putExtra("CurrentUser", auth.currentUser)
             }
             startActivity(intent)
-            finish(); // Call once you redirect to another activity
+            finish() // Call once you redirect to another activity
         } else {
             // not signed in
             showSignInOptions()
@@ -57,13 +89,12 @@ class LogInActivity : AppCompatActivity() {
             // Successfully signed in
             if (resultCode == RESULT_OK) {
                 val user = FirebaseAuth.getInstance().currentUser
-                Toast.makeText(this, "Welcome! " + user!!.displayName!!, Toast.LENGTH_LONG).show()
                 val intent = Intent(this, MainActivity::class.java).apply {
                     putExtra("CurrentUser", user)
                 }
                 startActivity(intent)
-                finish(); // Call once you redirect to another activity
-                Toast.makeText(this, "Welcome! " + user.displayName!!, Toast.LENGTH_LONG).show()
+                finish() // Call once you redirect to another activity
+                Toast.makeText(this, "Welcome! " + user?.displayName!!, Toast.LENGTH_LONG).show()
             } else {
                 // Sign in failed. If response is null the user canceled the
                 // sign-in flow using the back button. Otherwise check
--- app/src/main/java/com/android/itrip/MainActivity.kt
+++ app/src/main/java/com/android/itrip/MainActivity.kt
@@ -1,14 +1,29 @@
 package com.android.itrip
 
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import androidx.databinding.DataBindingUtil
 import com.android.itrip.databinding.ActivityMainBinding
+import com.android.volley.Request
+import com.android.volley.Response
+import com.android.volley.toolbox.JsonObjectRequest
+import com.android.volley.toolbox.StringRequest
+import com.android.volley.toolbox.Volley
+import com.google.firebase.auth.FirebaseAuth
+import com.squareup.picasso.Picasso
+import org.json.JSONObject
+import java.util.logging.Logger
+import androidx.core.app.ComponentActivity.ExtraData
+import androidx.core.content.ContextCompat.getSystemService
+import android.icu.lang.UCharacter.GraphemeClusterBreak.T
+import com.android.itrip.util.NukeSSLCerts
 
 
 class MainActivity : AppCompatActivity() {
 
+    val logger = Logger.getLogger(MainActivity::class.java.name)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -17,7 +32,56 @@ class MainActivity : AppCompatActivity() {
         val currentUser =
             intent.getParcelableExtra<com.google.firebase.auth.FirebaseUser>("CurrentUser")
 
+
+        currentUser?.providerData?.forEach {
+            logger.info("Sign-in provider: " + it.providerId)
+            logger.info("  Provider-specific UID: " + it.uid)
+            logger.info("  Name: " + it.displayName)
+            logger.info("  Email: " + it.email)
+            logger.info("  Photo URL: " + it.photoUrl)
+        }
+
+        if (currentUser != null) {
+            logger.info("currentUser idToken: " + currentUser.getIdToken(true))
+            logger.info("currentUser idToken: " + currentUser.getIdToken(false))
+            logger.info("currentUser displayName: " + currentUser.displayName)
+            logger.info("currentUser uid: " + currentUser.uid)
+            logger.info("currentUser email: " + currentUser.email)
+            logger.info("currentUser isEmailVerified: " + currentUser.isEmailVerified)
+            logger.info("currentUser photoUrl: " + currentUser.photoUrl)
+        }
+
+
+        Picasso.get().load(currentUser.photoUrl).into(binding.imageView)
+
+        binding.logOutButton.setOnClickListener {
+            FirebaseAuth.getInstance().signOut()
+            startActivity(Intent(this, LogInActivity::class.java))
+            finish()
+        }
+//        binding.logOutButton.setOnClickListener {
+//            getApiToken(null)
+//        }
+
         binding.currentUser = currentUser
     }
+
+//    fun getApiToken(auth: FirebaseAuth?) {
+//        val queue = Volley.newRequestQueue(this)
+//        val base_api_url = resources.getString(R.string.base_api_url)
+//        val url = base_api_url + "token/"
+//
+//        val updateJsonobj = JSONObject()
+//        updateJsonobj.put("username", "admin")
+//        updateJsonobj.put("password", "SlaidTeam123")
+//
+//        val jsonObjectRequest = JsonObjectRequest(Request.Method.POST, url, updateJsonobj,
+//            Response.Listener { response ->
+//                logger.info("Response: %s".format(response.toString()))
+//            },
+//            Response.ErrorListener { response -> logger.info("That didn't work! " + response.message) })
+//
+//        queue.add(jsonObjectRequest)
+//    }
 
 }
