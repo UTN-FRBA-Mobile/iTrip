85b1accb9bddfe74bf78e18032ac3a8179692154
giuliano64 <fgceliberti@gmail.com>
2019-10-16 22:40:44 -0300

    Restyling of home screen

--- app/src/main/java/com/android/itrip/fragments/HomeFragment.kt
+++ app/src/main/java/com/android/itrip/fragments/HomeFragment.kt
@@ -1,45 +1,65 @@
 package com.android.itrip.fragments
 
 
+import android.app.Application
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.view.View.INVISIBLE
+import android.view.View.VISIBLE
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
+import androidx.recyclerview.widget.DefaultItemAnimator
+import androidx.recyclerview.widget.LinearLayoutManager
 import com.android.itrip.R
+import com.android.itrip.adapters.TravelAdapter
 import com.android.itrip.databinding.FragmentHomeBinding
 import com.android.itrip.fragments.HomeFragmentDirections.Companion.actionHomeFragmentToCreateTravelFragment
-import com.android.itrip.fragments.HomeFragmentDirections.Companion.actionHomeFragmentToQuizInfoFragment
-import com.android.itrip.fragments.HomeFragmentDirections.Companion.actionHomeFragmentToTravelsFragment
+import com.android.itrip.services.TravelService
+import java.util.logging.Logger
 
 class HomeFragment : Fragment() {
 
+    private val logger = Logger.getLogger(this::class.java.name)
+    private var travelAdapter = TravelAdapter()
+    private lateinit var application: Application
+    private lateinit var binding: FragmentHomeBinding
+
     override fun onCreateView(
-        inflater: LayoutInflater, container: ViewGroup?,
+        inflater: LayoutInflater,
+        container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        val binding: FragmentHomeBinding = DataBindingUtil.inflate(
-            inflater, R.layout.fragment_home, container, false
-        )
-
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_home, container, false)
         binding.createTravel.setOnClickListener { view: View ->
             view.findNavController()
                 .navigate(actionHomeFragmentToCreateTravelFragment())
         }
-
-        binding.getTravels.setOnClickListener { view: View ->
-            view.findNavController()
-                .navigate(actionHomeFragmentToTravelsFragment())
-        }
-
-        binding.quiz.setOnClickListener { view: View ->
-            view.findNavController()
-                .navigate(actionHomeFragmentToQuizInfoFragment())
-        }
-
+        application = requireNotNull(this.activity).application
+        getTravels()
         return binding.root
+    }
+
+    private fun getTravels() {
+        binding.recyclerviewTravels.layoutManager = LinearLayoutManager(application)
+        binding.recyclerviewTravels.itemAnimator = DefaultItemAnimator()
+        binding.recyclerviewTravels.adapter = travelAdapter
+        TravelService.getTravels({ travels ->
+            if (travels.isNotEmpty()) {
+                binding.linearlayoutNoTravels.visibility = INVISIBLE
+                travelAdapter.replaceItems(travels)
+            } else {
+                binding.linearlayoutNoTravels.visibility = VISIBLE
+            }
+        }, { error ->
+            logger.info("Failed to get travels: " + error.message)
+            Toast
+                .makeText(this.context, "Hubo un problema, intente de nuevo", Toast.LENGTH_SHORT)
+                .show()
+        })
     }
 
 }
