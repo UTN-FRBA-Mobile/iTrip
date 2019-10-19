128c62dabd1a9f80b73e6b5d17e76fb901dc2e83
giuliano64 <fgceliberti@gmail.com>
2019-10-19 12:50:37 -0300

    Improving navigation controller from drawer

--- app/src/main/java/com/android/itrip/MainActivity.kt
+++ app/src/main/java/com/android/itrip/MainActivity.kt
@@ -2,43 +2,131 @@ package com.android.itrip
 
 
 import android.content.Intent
+import android.content.res.Configuration
 import android.os.Bundle
+import android.view.MenuItem
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
+import androidx.core.view.GravityCompat
 import androidx.databinding.DataBindingUtil
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
 import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.NavigationUI
 import com.android.itrip.databinding.ActivityMainBinding
+import com.android.itrip.databinding.AppBarHeaderBinding
 import com.android.itrip.services.ApiService
 import com.android.itrip.services.QuizService
+import com.google.android.material.navigation.NavigationView
 import com.google.firebase.auth.FirebaseAuth
-import com.squareup.picasso.Picasso
+import com.google.firebase.auth.FirebaseUser
 import kotlinx.android.synthetic.main.activity_main.*
-import java.util.logging.Logger
 
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), NavigationView.OnNavigationItemSelectedListener {
 
-    private val logger = Logger.getLogger(this::class.java.name)
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var toolbar: Toolbar
+    private lateinit var drawerLayout: DrawerLayout
+    private lateinit var drawerToggle: ActionBarDrawerToggle
+    private lateinit var navigationView: NavigationView
+    private lateinit var navController: NavController
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        val binding: ActivityMainBinding =
-            DataBindingUtil.setContentView(this, R.layout.activity_main)
-        val currentUser =
-            intent.getParcelableExtra<com.google.firebase.auth.FirebaseUser>("CurrentUser")
-
         ApiService.setContext(this)
-
-        Picasso.get().load(currentUser.photoUrl).placeholder(R.drawable.ic_person_primary_24dp)
-            .error(R.drawable.ic_person_primary_24dp).into(binding.imageView)
-
-        binding.logOutButton.setOnClickListener {
-            FirebaseAuth.getInstance().signOut()
-            startActivity(Intent(this, LogInActivity::class.java))
-            finish()
-        }
-
+        bindings()
+        initDrawer()
+        initNavigation()
         QuizService.getQuestionsVerify({ answered: Boolean -> quizAnswered(answered) }, {})
-        binding.currentUser = currentUser
+    }
+
+    private fun bindings() {
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        drawerLayout = binding.drawerLayout
+        toolbar = binding.appBar as Toolbar
+        navigationView = binding.navigationView
+    }
+
+    private fun initDrawer() {
+        drawerToggle = ActionBarDrawerToggle(
+            this,
+            drawerLayout,
+            toolbar,
+            R.string.drawer_open,
+            R.string.drawer_close
+        )
+        drawerLayout.addDrawerListener(drawerToggle)
+    }
+
+    private fun initNavigation() {
+        // settea el toolbar y el usuario logueado en el menu
+        setSupportActionBar(toolbar)
+        supportActionBar?.setDisplayHomeAsUpEnabled(true)
+        supportActionBar?.setHomeButtonEnabled(true)
+        val bindingAppBar: AppBarHeaderBinding = DataBindingUtil.inflate(
+            layoutInflater, R.layout.app_bar_header, navigationView, false
+        )
+        val currentUser = intent.getParcelableExtra<FirebaseUser>("CurrentUser")
+        bindingAppBar.currentUser = currentUser
+        // settea navigation controller y bindings del navigation view
+        navController = Navigation.findNavController(this, R.id.navHostFragment)
+        NavigationUI.setupActionBarWithNavController(this, navController, drawerLayout)
+        NavigationUI.setupWithNavController(navigationView, navController)
+        navigationView.addHeaderView(bindingAppBar.root)
+        navigationView.setNavigationItemSelectedListener(this)
+    }
+
+    override fun onPostCreate(savedInstanceState: Bundle?) {
+        super.onPostCreate(savedInstanceState)
+        drawerToggle.syncState()
+    }
+
+    override fun onConfigurationChanged(newConfig: Configuration) {
+        super.onConfigurationChanged(newConfig)
+        drawerToggle.onConfigurationChanged(newConfig)
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        when (item.itemId) {
+            R.id.home -> {
+                drawerLayout.openDrawer(GravityCompat.START)
+                return true
+            }
+        }
+        return super.onOptionsItemSelected(item)
+    }
+
+    override fun onSupportNavigateUp(): Boolean {
+        return NavigationUI.navigateUp(
+            Navigation.findNavController(this, R.id.navHostFragment),
+            drawerLayout
+        )
+    }
+
+    override fun onBackPressed() {
+        if (drawerLayout.isDrawerOpen(GravityCompat.START)) {
+            drawerLayout.closeDrawer(GravityCompat.START)
+        } else {
+            super.onBackPressed()
+        }
+    }
+
+    override fun onNavigationItemSelected(item: MenuItem): Boolean {
+        item.isChecked = true
+        drawerLayout.closeDrawer(GravityCompat.START)
+        when (item.itemId) {
+            R.id.drawer_menu_logout -> {
+                FirebaseAuth.getInstance().signOut()
+                startActivity(Intent(this, LogInActivity::class.java))
+                finish()
+            }
+            R.id.drawer_menu_create_travel -> navController.navigate(R.id.createTravelFragment)
+            R.id.drawer_menu_travels -> navController.navigate(R.id.homeFragment)
+        }
+        return true
     }
 
     private fun quizAnswered(answered: Boolean) {
--- app/src/main/java/com/android/itrip/fragments/CreateTravelFragment.kt
+++ app/src/main/java/com/android/itrip/fragments/CreateTravelFragment.kt
@@ -1,51 +1,24 @@
 package com.android.itrip.fragments
 
-
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import android.widget.Toast
 import androidx.databinding.DataBindingUtil
 import androidx.fragment.app.Fragment
 import androidx.navigation.findNavController
 import com.android.itrip.R
-import com.android.itrip.database.Destination
 import com.android.itrip.databinding.FragmentCreateTravelBinding
-import java.util.logging.Logger
 
-/**
- * A simple [Fragment] subclass.
- */
 class CreateTravelFragment : Fragment() {
-
-
-    private val logger = Logger.getLogger(this::class.java.name)
-    private lateinit var destinations: List<Destination>
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-
         val binding: FragmentCreateTravelBinding = DataBindingUtil.inflate(
             inflater, R.layout.fragment_create_travel, container, false
         )
-
-        this.arguments!!.get("destinations")?.let {
-            destinations = this.arguments!!.get("destinations") as List<Destination>
-            var textMessage = ""
-            destinations.forEach {
-                textMessage = textMessage + it.name + ", "
-            }
-            textMessage += "agregadas."
-            if (destinations.isNotEmpty()) Toast.makeText(
-                context,
-                textMessage,
-                Toast.LENGTH_SHORT
-            ).show()
-        }
-
         binding.createTravel.setOnClickListener { view: View ->
             view.findNavController()
                 .navigate(CreateTravelFragmentDirections.actionCreateTravelFragmentToActivitiesHomeFragment())
@@ -56,6 +29,5 @@ class CreateTravelFragment : Fragment() {
         }
         return binding.root
     }
-
 
 }
