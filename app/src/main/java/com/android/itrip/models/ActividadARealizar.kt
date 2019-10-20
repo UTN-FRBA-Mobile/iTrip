644e7b5458e527bb458c06984afed76b6bf13df7
Facundo Aita <facundo91@gmail.com>
2019-10-20 19:38:55 -0300

    Schedule WIP

--- app/src/main/java/com/android/itrip/models/ActividadARealizar.kt
+++ app/src/main/java/com/android/itrip/models/ActividadARealizar.kt
@@ -1,15 +1,36 @@
 package com.android.itrip.models
 
 import java.io.Serializable
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 data class ActividadARealizar(
     var id: Long = 0L,
 
-    var dia: String = "",
+    var dia: Calendar,
 
-    var bucket_inicio: Int = 0,
+    var bucket_inicio: Int,
 
     var detalle_actividad: Actividad
 
 ) : Serializable
+
+data class ActividadARealizarCreator(
+    val id: Long,
+    var dia: String,
+
+    var bucket_inicio: Int,
+
+    var detalle_actividad: Actividad
+) : Serializable {
+
+    fun actividadARealizar(): ActividadARealizar {
+        return ActividadARealizar(
+            id,
+            Calendar.getInstance().apply { time = SimpleDateFormat("yyyy-MM-dd").parse(dia) },
+            bucket_inicio,
+            detalle_actividad
+        )
+    }
+}
