00cfb29402cefa7a16af509190b42d7b1541521d
giuliano64 <fgceliberti@gmail.com>
2019-10-25 00:44:28 -0300

    Remove logger

--- app/src/main/java/com/android/itrip/services/QuizService.kt
+++ app/src/main/java/com/android/itrip/services/QuizService.kt
@@ -7,12 +7,10 @@ import com.android.itrip.models.Quiz
 import com.android.volley.VolleyError
 import com.google.gson.Gson
 import org.json.JSONObject
-import java.util.logging.Logger
 
 
 object QuizService : Service() {
 
-    private val logger = Logger.getLogger(this::class.java.name)
     private val gson = Gson()
 
     override fun onBind(intent: Intent?): IBinder? {
@@ -35,7 +33,6 @@ object QuizService : Service() {
         errorHandler: (VolleyError) -> Unit
     ) {
         val url = "questions/"
-        logger.info(gson.toJson(quiz))
         val json = JSONObject(gson.toJson(quiz))
         ApiService.post(url, json, {
             responseHandler()
