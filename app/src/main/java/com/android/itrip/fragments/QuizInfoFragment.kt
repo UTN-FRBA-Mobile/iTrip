cb9183c362e1c8b4e659623130f9a959cf730791
giuliano64 <fgceliberti@gmail.com>
2019-11-06 00:34:31 -0300

    Fix Quiz behavior

--- app/src/main/java/com/android/itrip/fragments/QuizInfoFragment.kt
+++ app/src/main/java/com/android/itrip/fragments/QuizInfoFragment.kt
@@ -1,11 +1,13 @@
 package com.android.itrip.fragments
 
 
+import android.content.Context
 import android.os.Build
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.view.inputmethod.InputMethodManager
 import android.widget.EditText
 import android.widget.RelativeLayout
 import androidx.appcompat.app.AlertDialog
@@ -79,51 +81,44 @@ class QuizInfoFragment : Fragment() {
 
     private fun bindings() {
         binding.textinputlayoutQuizInfoGenero.editText.apply {
-            keyListener = null
-            isFocusableInTouchMode = false
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                focusable = View.NOT_FOCUSABLE
-            }
+            setFocusableBehavior()
             setOnClickListener {
-                it.clearFocus()
-                setText(" ") // hack (do not remove)
+                closeKeyboard(hasFocus = false)
                 setAlertDialog(getString(DIALOG_TITLE_GEN), this, quizViewModel.genero)
             }
         }
+        // when on focus changes it closes the keyboard because the other inputs cant be written
+        binding.textinputlayoutQuizInfoEdad.editText.setOnFocusChangeListener { view, hasFocus ->
+            closeKeyboard(hasFocus)
+        }
         binding.textinputlayoutQuizInfoEstadocivil.editText.apply {
-            keyListener = null
-            isFocusableInTouchMode = false
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                focusable = View.NOT_FOCUSABLE
-            }
+            setFocusableBehavior()
             setOnClickListener {
-                it.clearFocus()
+                closeKeyboard(false)
                 setAlertDialog(getString(DIALOG_TITLE_EC), this, quizViewModel.estado_civil)
             }
         }
         binding.textinputlayoutQuizInfoEstudios.editText.apply {
-            keyListener = null
-            isFocusableInTouchMode = false
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                focusable = View.NOT_FOCUSABLE
-            }
+            setFocusableBehavior()
             setOnClickListener {
-                it.clearFocus()
+                closeKeyboard(false)
                 setAlertDialog(getString(DIALOG_TITLE_EST), this, quizViewModel.nivel_de_estudios)
             }
         }
         binding.textinputlayoutQuizInfoOcupacion.editText.apply {
-            keyListener = null
-            isFocusableInTouchMode = false
-            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-                focusable = View.NOT_FOCUSABLE
-            }
+            setFocusableBehavior()
             setOnClickListener {
-                it.clearFocus()
+                closeKeyboard(false)
                 setAlertDialog(getString(DIALOG_TITLE_OCU), this, quizViewModel.ocupacion)
             }
         }
-        binding.floatingactionbuttonQuizInfo.setOnClickListener { view -> createQuiz(view) }
+        binding.floatingactionbuttonQuizInfo.setOnClickListener { view ->
+            // hack (do not remove)
+            if (binding.textinputlayoutQuizInfoOtrogenero.editText.text.toString() == " ") {
+                binding.textinputlayoutQuizInfoOtrogenero.editText.setText("")
+            }
+            createQuiz(view)
+        }
     }
 
     private fun setAlertDialog(title: String, input: EditText, elements: List<Answer>) {
@@ -140,17 +135,40 @@ class QuizInfoFragment : Fragment() {
 
     private fun handleOtherGender(option: Int) {
         if (option == 2) {
-            binding.textinputlayoutQuizInfoOtrogenero.visibility = View.VISIBLE
-            binding.textinputedittextQuizInfoEdad.layoutParams
+            with(binding.textinputlayoutQuizInfoOtrogenero) {
+                visibility = View.VISIBLE
+                editText.setText(" ") // hack (do not remove)
+                requestFocus()
+            }
+            binding.textinputlayoutQuizInfoEdad.layoutParams
                 .let { it as RelativeLayout.LayoutParams }
                 .addRule(RelativeLayout.BELOW, R.id.textinputlayout_quiz_info_otrogenero)
         } else {
             binding.textinputlayoutQuizInfoOtrogenero.visibility = View.GONE
-            binding.textinputedittextQuizInfoEdad.layoutParams
+            binding.textinputlayoutQuizInfoEdad.layoutParams
                 .let { it as RelativeLayout.LayoutParams }
                 .addRule(RelativeLayout.BELOW, R.id.textinputlayout_quiz_info_otrogenero)
-            binding.textinputlayoutQuizInfoOtrogenero.editText.setText(" ") // hack (do not remove)
+            binding.textinputlayoutQuizInfoOtrogenero.editText.setText("<hack>") // hack (do not remove)
         }
+    }
+
+    private fun closeKeyboard(hasFocus: Boolean) {
+        if (!hasFocus) {
+            binding.root.let { v ->
+                val imm =
+                    context!!.getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(v.windowToken, 0)
+            }
+            binding.textinputlayoutQuizInfoEdad.editText.clearFocus()
+            binding.textinputlayoutQuizInfoOtrogenero.editText.clearFocus()
+        }
+    }
+
+    private fun EditText.setFocusableBehavior() {
+        // avoid double touching when interacting with the input
+        keyListener = null
+        isFocusableInTouchMode = false
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) focusable = View.NOT_FOCUSABLE
     }
 
     private fun createQuiz(view: View) {
@@ -158,7 +176,7 @@ class QuizInfoFragment : Fragment() {
         if (binding.form.isValid) {
             val generoValue = binding.textinputlayoutQuizInfoGenero.editText.text.toString()
             val otroGeneroValue = binding.textinputlayoutQuizInfoOtrogenero.editText.text.toString()
-            val edadValue = binding.textinputedittextQuizInfoEdad.editText.text.toString()
+            val edadValue = binding.textinputlayoutQuizInfoEdad.editText.text.toString()
             val ecValue = binding.textinputlayoutQuizInfoEstadocivil.editText.text.toString()
             val estudiosValue = binding.textinputlayoutQuizInfoEstudios.editText.text.toString()
             val ocupacionValue = binding.textinputlayoutQuizInfoOcupacion.editText.text.toString()
